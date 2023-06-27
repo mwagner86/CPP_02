5906aee20239ccdc2c54d3da72d3b3eea057d8a5
mwagner <mwagner@students.42wolfsburg.de>
2023-06-27 10:27:35 +0200

    ex01 without header and tests

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -3,3 +3,57 @@
 //
 
 #include "Fixed.hpp"
+
+Fixed::Fixed() {
+	std::cout << "Default Constructor called" << std::endl;
+	this->_fpn_value= 0;
+}
+
+Fixed::Fixed(Fixed const &src) {
+	std::cout << "Copy constructor called" << std::endl;
+	this->_fpn_value = src._fpn_value;
+}
+
+Fixed::Fixed(int const src) {
+	std::cout << "Int constructor called" << std::endl;
+	this->_fpn_value = src << Fixed::_digits;
+}
+
+Fixed::Fixed(float const input) {
+	std::cout << "Float constructor called\n";
+	this->_fpn_value = (int)roundf(input * (1 << Fixed::_digits));
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called" << std::endl;
+}
+
+int	Fixed::getRawBits() const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_fpn_value);
+}
+
+void	Fixed::setRawBits(int const raw) {
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_fpn_value = raw;
+}
+
+Fixed &	Fixed::operator=(Fixed const &rhs) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	if ( this != &rhs )
+		this->_fpn_value = rhs._fpn_value;
+	return *this;
+}
+
+std::ostream &	operator<<( std::ostream & o, Fixed const & i ) {
+	o << i.toFloat();
+	return o;
+}
+
+float	Fixed::toFloat() const {
+	return ((float)this->_fpn_value / (1 << Fixed::_digits));
+}
+
+int		Fixed::toInt() const {
+	return (this->_fpn_value >> Fixed::_digits);
+}
--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -4,11 +4,32 @@
 
 #ifndef EX01_FIXED_HPP
 #define EX01_FIXED_HPP
-
+#include <iostream>
+#include <cmath>
 
 class Fixed {
 
+public:
+
+	Fixed();
+	Fixed(Fixed const & src);
+	Fixed(int const src);
+	Fixed(float const src);
+	~Fixed();
+
+	Fixed &	operator=( Fixed const & rhs );
+	int		getRawBits() const;
+	void	setRawBits( int const raw );
+	float	toFloat() const;
+	int		toInt() const;
+
+private:
+
+	int 				_fpn_value;
+	static int const	_digits = 8;
+
 };
 
+std::ostream &		operator<<( std::ostream & o, Fixed const & i );
 
 #endif //EX01_FIXED_HPP
